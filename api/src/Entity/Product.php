380d66d247a434079cff6ff8bf1e962ace734c05
Andrii Vozniuk <lymphbizkit@gmail.com>
2024-05-19 03:23:16 +0300

    Simple frond and back filtration.

--- api/src/Entity/Product.php
+++ api/src/Entity/Product.php
@@ -2,42 +2,143 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\NumericFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\RangeFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use App\Repository\ProductRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Range;
 
+#[ApiResource(
+    collectionOperations: [
+        "get" => [
+            "method" => "GET",
+            "normalization_context" => ["groups" => ["get:collection:product"]],
+            "openapi_context" => [
+                "parameters" => [
+                    [
+                        "name" => "page",
+                        "in" => "query",
+                        "required" => true,
+                        "schema" => ["type" => "integer"],
+                    ]
+                ]
+            ]
+        ],
+        "post" => [
+            "method" => "POST",
+            "denormalization_context" => ["groups" => ["post:collection:product"]],
+            "normalization_context" => ["groups" => ["empty"]],
+        ]
+    ],
+    itemOperations: [
+        "get" => [
+            "method" => "GET",
+            "normalization_context" => ["groups" => ["get:item:product"]]
+        ]
+    ]
+)]
+#[ApiFilter(SearchFilter::class, properties: ["productName" => "partial"])]
+#[ApiFilter(NumericFilter::class, properties: ['code'])]
+#[ApiFilter(OrderFilter::class, properties: [
+    "id" => "DESC",
+    "code" => "DESC",
+    "price" => "DESC",
+    "productName" => "ASC",
+    "type.typeName" => "ASC",
+])]
+#[ApiFilter(RangeFilter::class, properties: ["product.id"])]
 #[ORM\Entity(repositoryClass: ProductRepository::class)]
 class Product
 {
+    /**
+     * @var int|null
+     */
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups([
+        "get:collection:product",
+        "get:item:product",
+        "post:collection:product"
+    ])]
     private ?int $id = null;
 
+    /**
+     * @var int|null
+     */
     #[ORM\Column]
+    #[NotBlank]
     #[Range(min: 1, max: 10)]
+    #[Groups([
+        "get:collection:product",
+        "get:item:product",
+        "post:collection:product"
+    ])]
     private ?int $code = null;
 
+    /**
+     * @var string|null
+     */
+    #[NotBlank]
     #[ORM\Column(length: 255)]
+    #[Groups([
+        "get:collection:product",
+        "get:item:product",
+        "post:collection:product"
+    ])]
     private ?string $productName = null;
 
+    /**
+     * @var string|null
+     */
+    #[NotBlank]
     #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    #[Range(min: 0)]
+    #[Groups([
+        "get:collection:product",
+        "get:item:product",
+        "post:collection:product"
+    ])]
     private ?string $price = null;
 
+    /**
+     * @var ProductType|null
+     */
     #[ORM\ManyToOne(inversedBy: 'products')]
+    #[Groups([
+        "get:collection:product",
+        "get:item:product",
+        "post:collection:product"
+    ])]
     private ?ProductType $type = null;
 
+    /**
+     * @return int|null
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    /**
+     * @return int|null
+     */
     public function getCode(): ?int
     {
         return $this->code;
     }
 
+    /**
+     * @param int $code
+     * @return $this
+     */
     public function setCode(int $code): static
     {
         $this->code = $code;
@@ -45,11 +146,18 @@ class Product
         return $this;
     }
 
+    /**
+     * @return string|null
+     */
     public function getProductName(): ?string
     {
         return $this->productName;
     }
 
+    /**
+     * @param string $productName
+     * @return $this
+     */
     public function setProductName(string $productName): static
     {
         $this->productName = $productName;
@@ -57,11 +165,18 @@ class Product
         return $this;
     }
 
+    /**
+     * @return string|null
+     */
     public function getPrice(): ?string
     {
         return $this->price;
     }
 
+    /**
+     * @param string $price
+     * @return $this
+     */
     public function setPrice(string $price): static
     {
         $this->price = $price;
@@ -69,11 +184,18 @@ class Product
         return $this;
     }
 
+    /**
+     * @return ProductType|null
+     */
     public function getType(): ?ProductType
     {
         return $this->type;
     }
 
+    /**
+     * @param ProductType|null $type
+     * @return $this
+     */
     public function setType(?ProductType $type): static
     {
         $this->type = $type;
--- api/src/Entity/ProductType.php
+++ api/src/Entity/ProductType.php
@@ -2,44 +2,117 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\ProductTypeRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
+#[ApiResource(
+    collectionOperations: [
+        "get" => [
+            "method" => "GET",
+            "normalization_context" => ["groups" => ["get:collection:productType"]],
+            "openapi_context" => [
+                "parameters" => [
+                    [
+                        "name" => "page",
+                        "in" => "query",
+                        "required" => true,
+                        "schema" => ["type" => "integer"],
+                    ]
+                ]
+            ]
+        ],
+        "post" => [
+            "method" => "POST",
+            "denormalization_context" => ["groups" => ["post:collection:productType"]],
+            "normalization_context" => ["groups" => ["empty"]],
+        ]
+    ],
+    itemOperations: [
+        "get" => [
+            "method" => "GET",
+            "normalization_context" => ["groups" => ["get:item:productType"]]
+        ]
+    ],
+)]
 #[ORM\Entity(repositoryClass: ProductTypeRepository::class)]
 class ProductType
 {
+    /**
+     * @var int|null
+     */
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
+    /**
+     * @var string|null
+     */
+    #[NotBlank]
     #[ORM\Column(length: 255)]
+    #[Groups([
+        "get:collection:productType",
+        "get:item:productType",
+        "post:collection:productType",
+        "get:collection:product",
+        "get:item:product",
+    ])]
     private ?string $typeName = null;
 
+    /**
+     * @var string|null
+     */
+    #[NotBlank]
     #[ORM\Column(type: Types::TEXT, nullable: true)]
+    #[Groups([
+        "get:collection:productType",
+        "get:item:productType",
+        "post:collection:productType",
+        "get:collection:product",
+        "get:item:product",
+    ])]
     private ?string $description = null;
 
+    /**
+     * @var Collection
+     */
     #[ORM\OneToMany(mappedBy: 'type', targetEntity: Product::class)]
     private Collection $products;
 
+    /**
+     *
+     */
     public function __construct()
     {
         $this->products = new ArrayCollection();
     }
 
+    /**
+     * @return int|null
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    /**
+     * @return string|null
+     */
     public function getTypeName(): ?string
     {
         return $this->typeName;
     }
 
+    /**
+     * @param string $typeName
+     * @return $this
+     */
     public function setTypeName(string $typeName): static
     {
         $this->typeName = $typeName;
@@ -47,11 +120,18 @@ class ProductType
         return $this;
     }
 
+    /**
+     * @return string|null
+     */
     public function getDescription(): ?string
     {
         return $this->description;
     }
 
+    /**
+     * @param string|null $description
+     * @return $this
+     */
     public function setDescription(?string $description): static
     {
         $this->description = $description;
@@ -67,6 +147,10 @@ class ProductType
         return $this->products;
     }
 
+    /**
+     * @param Product $product
+     * @return $this
+     */
     public function addProduct(Product $product): static
     {
         if (!$this->products->contains($product)) {
@@ -77,6 +161,10 @@ class ProductType
         return $this;
     }
 
+    /**
+     * @param Product $product
+     * @return $this
+     */
     public function removeProduct(Product $product): static
     {
         if ($this->products->removeElement($product)) {
